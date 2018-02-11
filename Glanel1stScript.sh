a4cb5b557b58527ffb0270e3047e1517ce97ecbb
Glanel Houenouvi <grenack@gmail.com>
2018-02-11 00:35:16 -0600

    Glanel1stScript.sh

---
+++ Glanel1stScript.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+#Week2 Homework, Chapter2
+#GlanelHouenouvi
+#CSCI2461-70 Computer Networking Linux
+#Create This Sentence
+echo "Hello World"
+#Create File1 File2
+touch File1
+touch File2
+#Copy File1 in File2
+cp File1 File2
+#Show the content for detailed long listing
+ls -l
+#Move File1 in File3
+mv File1 File3
+#Making Directory
+mkdir Dir
+#Change Directory to the home 
+cd
+#Presenting data from  /dev directory
+ls -l /dev > File1 |cat File1
+
+# Thank you 
